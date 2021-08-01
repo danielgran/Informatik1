f86bbe6b09a5e0927eb0599943989732f3821285
Daniel Gran <6535118+danielgran@users.noreply.github.com>
2021-08-01 10:43:55 +0200

    Init

--- tests/tests.c
+++ tests/tests.c
@@ -1,24 +1,10 @@
 #include <stdio.h>
 #include <math.h>
+#include <time.h>
 
 int main()
 {
-    int eingabe = 0;
-    fflush(stdin);
+    int $a= $a + +-+-+- -5;
 
-    eingabe = 100;
-
-    printf("Oktal: %o\n", eingabe);
-    printf("Hexa: %x\n", eingabe);
-    printf("Dual: ");
-
-    for (int i = 4*8-1; i >= 0; i--)
-    {
-        printf("%d", ((int)pow((double)2, (double)i)) && (eingabe & (int)pow((double)2,(double)i)));
-    }
-
-
-
-
-    return 0;
+    printf("%d", $a);    
 }
